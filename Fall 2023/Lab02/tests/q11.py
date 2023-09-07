708960cf10cabaf998f8dc166edf8dcc1d8303b4
Jonathan M. Smith <jmsmith@quantum-curiosity.com>
2023-09-07 07:51:48 -0400

    Update q11.py

--- Fall 2023/Lab02/tests/q11.py
+++ Fall 2023/Lab02/tests/q11.py
@@ -9,7 +9,8 @@ test = {
           >>> # Hint: You can write the sine of 1.5*pi as:
           >>> #   math.sin(1.5 * math.pi)
           >>> import math
-          >>> round(sine_of_pi_over_four, 8)
+          >>> import numpy as np
+          >>> np.round(sine_of_pi_over_four, 8) 
           0.70710678
           """,
           'hidden': False,
