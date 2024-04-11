3a5ef65883384d066ce9433dbcf57914f3fdf504
Jonathan M. Smith <jmsmith@quantum-curiosity.com>
2024-04-11 07:56:25 -0400

    Update Class 024 example

--- Spring 2024/Class_Examples/EDS.py
+++ Spring 2024/Class_Examples/EDS.py
@@ -32,3 +32,52 @@ def ptrend(tbl,datec,datac,fmtdate="%b-%Y"):
     plt.gcf().autofmt_xdate()
     days= (datetrend.max()-datetrend.min()).astype('timedelta64[D]')/np.timedelta64(1, 'D')
     return days
+### Molecular Tools
+import py3Dmol
+
+from rdkit import Chem
+from rdkit.Chem.Draw import IPythonConsole #Needed to show molecules
+from rdkit.Chem.Draw.MolDrawing import MolDrawing, DrawingOptions #Only needed if modifying defaults
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+from rdkit import DataStructs
+# Options
+DrawingOptions.bondLineWidth=1.8
+
+
+def MolTo3DView(mol, size=(300, 300), style="stick", surface=True, opacity=0.5, label=True):
+    """Draw molecule in 3D
+    
+    Args:
+    ----
+        mol: rdMol, molecule to show
+        size: tuple(int, int), canvas size
+        style: str, type of drawing molecule
+               style can be 'line', 'stick', 'sphere', 'carton'
+        surface, bool, display SAS
+        opacity, float, opacity of surface, range 0.0-1.0
+    Return:
+    ----
+        viewer: py3Dmol.view, a class for constructing embedded 3Dmol.js views in ipython notebooks.
+    """
+    assert style in ('line', 'stick', 'sphere', 'carton')
+    mblock = Chem.MolToMolBlock(mol)
+    viewer = py3Dmol.view(width=size[0], height=size[1])
+    viewer.addModel(mblock, 'mol')
+    viewer.setStyle({style:{}})
+    if surface:
+        viewer.addSurface(py3Dmol.SAS, {'opacity': opacity})
+    viewer.zoomTo()
+    return viewer
+def smiles3D(smiles):
+    '''Convert SMILES to rdkit.Mol with 3D coordinates'''
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is not None:
+        mol = Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol)
+        AllChem.MMFFOptimizeMolecule(mol, maxIters=200)
+        viewer = MolTo3DView(mol, size=(600, 300), style='stick')
+        viewer.show()
+        return mol
+    else:
+        return None
