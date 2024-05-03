a9170183b3faaa259104736035e80044458b87dd
Jonathan Nyquist <103674665+Jonathan-Nyquist@users.noreply.github.com>
2024-05-03 15:44:13 -0400

    Working on Lab 0

---
+++ under_development/Lab00/tests/q4.py
@@ -0,0 +1,22 @@
+test = {
+    "name": "",
+    "points": 1,
+    "suites": [
+        {
+            "cases": [ 
+                {
+                    "code": r"""
+                    >>> c == "Temple Made!"
+                    True
+                    """,
+                    "hidden": True,
+                    "locked": False,
+                }, 
+            ],
+            "scored": True,
+            "setup": "",
+            "teardown": "",
+            "type": "doctest"
+        }
+    ]
+}
