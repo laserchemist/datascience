acd33a473c93c4810a78699d7534a354a765b0f4
Jonathan Nyquist <103674665+Jonathan-Nyquist@users.noreply.github.com>
2024-07-24 14:05:40 -0400

    Update to Lab08
    Added more comments and tests for open-ended questions.

---
+++ Summer 2024/Lab08/tests/q2_open_ended.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2 Open Ended',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+            'code': r"""
+             >>> # You need to write more than that!
+             >>> len(q2_answer) > 10
+             True
+             """
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': print('Note: this test only checks that you answered, not whether your answer is correct.'),
+      'type': 'doctest'
+    }
+  ]
+}
+
