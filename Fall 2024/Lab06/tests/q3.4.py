8ff1ce73bd226cb3f9b611cf72a4a09c957f2bf1
Jonathan M. Smith <jmsmith@quantum-curiosity.com>
2024-10-07 23:25:57 -0400

    Update Lab 06 with new dictionary question to examine RSV waste monitoring article

---
+++ Fall 2024/Lab06/tests/q3.4.py
@@ -0,0 +1,53 @@
+test = {
+  'name': 'q3.4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> ### Testing answer to open ended question if answer is not there or too short, fails
+          >>> test_open('Question: What are the most important words in the article?',notebook,60)==1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+          {
+          'code': r"""
+          >>> ### Testing answer to open ended question if answer is not there or too short, fails
+          >>> test_open('Question: Now what are the most important words in the article after removing stop words?',notebook,60)==1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+          {
+          'code': r"""
+          >>> ### Looking for last Table without stop words
+          >>> type(RSV_word_table_nostop)==tables.Table
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+         ,
+          {
+          'code': r"""
+          >>> ### Looking for rsv word count
+          >>> type(rsv)==int
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+          
+          
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': print('Note: this test only checks that you answered, not whether your answer is correct.'),
+      'type': 'doctest'
+    }
+  ]
+}
