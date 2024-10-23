4a58ede9551f74c60eef46835c65e59bdeefa8bc
Jonathan M. Smith <jmsmith@quantum-curiosity.com>
2024-10-23 16:51:17 -0400

    Update Lab08 open questions

--- Fall 2024/Lab08/tests/q1.py
+++ Fall 2024/Lab08/tests/q1.py
@@ -7,7 +7,19 @@ test = {
         {
           'code': r"""
           >>> ### Testing answer to open ended question if answer is not there or too short, fails
-          >>> test_open('What trend do you see in the data?',notebook,60)==1
+          >>> ### Questions is tested against standard solution for logical answers
+          >>> ck==1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+          {
+          'code': r"""
+          >>> ### Testing answer to open ended question if answer is not there or too short, fails
+          >>> score = test_open('What trend do you see in the data?',notebook,60)
+          >>> #print(f'Your answer was compared to and answer, receiving a score of {score:.2f}')
+          >>> score > .5
           True
           """,
           'hidden': False,
