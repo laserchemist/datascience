959ace8a439bfb1d78bff1802decf64cb6318afc
Jonathan M. Smith <jmsmith@quantum-curiosity.com>
2025-01-01 12:03:26 -0500

    Update Lab 04 using improved BETA from Fall 2025
    Remove divide by zero problem, distracting
    Bring in Wastewater which is key approach informing CDC and in the news even with recent Norovirus

--- Spring 2025/Lab04/EDS.py
+++ Spring 2025/Lab04/EDS.py
@@ -6,12 +6,6 @@ plt.style.use('ggplot')
 import matplotlib.dates as mdates
 from matplotlib import ticker
 import numpy as np
-import nbformat as nbf
-import glob
-import os
-from tqdm.notebook import tqdm, trange
-from sentence_transformers import CrossEncoder
-import torch
 def Tdate(date_string):
     return time.mktime(strptime(date_string, '%m/%d/%Y'))
 def FilterTdate(tbl,d1,d2,datec=0,fmtdate='%m/%d/%Y'):
@@ -105,59 +99,11 @@ def fplot(f,xrange,yrange):
     ax.tick_params(which='both', width=2)
     ax.tick_params(which='major', length=7)
     ax.tick_params(which='minor', length=4, width=.5,color='r')
-    plt.plot([x for x in np.arange(xrange[0],xrange[1],abs(xrange[1]-xrange[0])/50)],[f(x) for x in np.arange(xrange[0],xrange[1],abs(xrange[1]-xrange[0])/50)],lw=4, c='chartreuse',label='function')
+    step = abs(xrange[1]-xrange[0])/50
+    plt.plot([x for x in np.arange(xrange[0],xrange[1]+step,step)],[f(x) for x in np.arange(xrange[0],xrange[1]+step,step)],lw=4, c='chartreuse',label='function')
     ax.set_xlim(xrange)
     ax.set_ylim(yrange)
     plt.grid( which='major', color='g', linestyle='--',alpha=.15)
     plt.legend()
     plt.show()
-    return True
-def test_open(text,notebook,length):
-    response_length = -10
-    notebooks = glob.glob('*.ipynb')
-    notebook_new = max(notebooks, key=os.path.getmtime)
-    if notebook_new != notebook:
-        notebook = notebook_new
-    ntbk = nbf.read(notebook, nbf.NO_CONVERT)
-    for index, obj in enumerate(ntbk.cells):
-        if text in obj.source:
-            next_item = ntbk.cells[index + 1]
-            #print(f'Your response: {next_item.source}')
-            response_length = len(next_item.source)
-            break
-    if response_length<length:
-        #print(f'Your reponse to {text} is not sufficient ')
-        score =0
-    else:
-        #print(f'Your reponse to {text} has been checked that you answered but not for content ')
-        score =1
-    return score
-def test_open_score(text,notebook,length,file):
-    print('+++++\n','Checking your response relative to key ideas\n++++')
-    file = 'tests/'+file+'.txt'
-    with open(file, "r") as f:
-        answer=f.read()
-    model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2", default_activation_function=torch.nn.ReLU6())
-    response_length = -10
-    notebooks = glob.glob('*.ipynb')
-    notebook_new = max(notebooks, key=os.path.getmtime)
-    if notebook_new != notebook:
-        notebook = notebook_new
-    ntbk = nbf.read(notebook, nbf.NO_CONVERT)
-    ascore = 0
-    for index, obj in enumerate(ntbk.cells):
-        if text in obj.source:
-            next_item = ntbk.cells[index + 1]
-            print(f'Your response: {next_item.source}')
-            ascore = model.predict((answer, next_item.source))/6
-            print(f'Similarity of your answer to expected ideas: {ascore:.2f}\n')
-            response_length = len(next_item.source)
-            break
-    score = ascore
-    return score
-def check_open(file,notebook):
-    '''Read file with parameters'''
-    data = [line.strip() for line in open(file, "r")]
-    sc=test_open_score(data[1],notebook,60,data[-1])
-    score = (sc > 0.6)*1
-    return score
+    return 
--- Spring 2025/Lab04/tests/q6a.py
+++ Spring 2025/Lab04/tests/q6a.py
@@ -9,11 +9,17 @@ test = {
             'code': r"""
             >>> # Need create function ratio(x1,x2)
             >>> from datascience import *
-            >>> ratio(1,4) == 25.0
+            >>> round(ratio(1,4),2) < 0.4
             True
             """
-        }
-    
+        },
+         {
+            'code': r"""
+            >>> # Need to respond to question
+            >>> len(q6_answer) >20
+            True
+            """
+         } 
       ],
       'scored': True,
       'setup': '',
