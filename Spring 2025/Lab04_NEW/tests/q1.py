cf4c41f4633b4b7889a425748092f8607e30e4dc
Jonathan Nyquist <103674665+Jonathan-Nyquist@users.noreply.github.com>
2025-01-07 10:31:30 -0500

    Possible new version of Lab04

---
+++ Spring 2025/Lab04_NEW/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+
+        {
+            'code': r"""
+            >>> # Did you use descending=False?
+            >>> weights == make_array([6000, 1600,  200,  300,  350])
+            array([[ True,  True,  True,  True,  True]], dtype=bool)
+            """
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+   }
+  ]
+}
---
+++ Spring 2025/Lab04_NEW/tests/q5.py
@@ -0,0 +1,28 @@
+test = {
+  'name': 'Question 4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+            'code': r"""
+            >>> # Did you test the function yourself?
+            >>> sum_two_numbers(4, 5)
+            9
+            """
+        },
+        {
+            'code': r"""
+            >>> # Did you test the function yourself?
+            >>> sum_two_numbers(11, 39)
+            50
+            """
+        },
+       ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
